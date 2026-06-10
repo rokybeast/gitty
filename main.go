d1816f1e6b7daff5eb0ed5b02634b9c4dcb1fa54
Mohammed Sajid Shaik <sajid.shaik1186@gmail.com>
2026-06-10 10:36:29 +0530

    feat: made main.go to link everything

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"gitty/internal/ui"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func main() {
+	p := tea.NewProgram(ui.New(), tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "[error]: gitty error - %v\n", err)
+		os.Exit(1)
+	}
+}
